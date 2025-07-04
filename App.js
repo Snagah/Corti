55af1ac0c025e369bbe68f9cc4adb0f6713df279
Snagah <152746673+Snagah@users.noreply.github.com>
2025-07-04 22:16:03 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -91,4 +91,4 @@ function App() {
   ]);
 }
 
-ReactDOM.createRoot(document.getElementById("root")).render(React.createElement(App));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
